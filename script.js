40f29b909984ed88f74abf9697c590a90884e69a
Evgeny Tunnikov <tunnikov2013@yandex.ru>
2020-06-08 09:58:39 +0300

    сделал первове усложненное задание

---
+++ script.js
@@ -0,0 +1,53 @@
+    let num = 266219;
+
+    let str = num + '';
+
+    let sum = str.split('');
+
+    let composition = 1
+
+    for (let i = 0; i < sum.length; i++) {
+        composition *= sum[i];
+    }
+    console.log(composition);
+
+
+
+console.log(nam);
+
+    var nam = 4
+
+
+
+
+
+
+
+//  console.log(sam);
+
+// // var arr = ["Яблоко", "Апельсин", "Груша"];
+
+// //  sum.forEach(function(item, i, arr) {
+// //    console.log ( i + ": " + item + " (массив:" + arr + ")" );
+// //  });
+
+// var result = sam.reduce(function(sum, current) {
+//     return sum + current;
+//   }, 0);
+//   console.log(result);
+  
+
+
+
+
+
+// for (let i = 0; i < str.lenght; i++) {
+//     console.log(str[i]);
+    
+// }
+
+// console.log((''+num).split(''));
+
+// let result = num.reduce((sum, current) => sum + current, 0);
+
+// alert(result);
