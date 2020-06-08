c236a719ce05d1dbeddb12721be1fc807af3665a
Evgeny Tunnikov <tunnikov2013@yandex.ru>
2020-06-08 09:59:13 +0300

    сделал первове усложненное задание

--- script.js
+++ script.js
@@ -13,41 +13,4 @@
 
 
 
-console.log(nam);
 
-    var nam = 4
-
-
-
-
-
-
-
-//  console.log(sam);
-
-// // var arr = ["Яблоко", "Апельсин", "Груша"];
-
-// //  sum.forEach(function(item, i, arr) {
-// //    console.log ( i + ": " + item + " (массив:" + arr + ")" );
-// //  });
-
-// var result = sam.reduce(function(sum, current) {
-//     return sum + current;
-//   }, 0);
-//   console.log(result);
-  
-
-
-
-
-
-// for (let i = 0; i < str.lenght; i++) {
-//     console.log(str[i]);
-    
-// }
-
-// console.log((''+num).split(''));
-
-// let result = num.reduce((sum, current) => sum + current, 0);
-
-// alert(result);
