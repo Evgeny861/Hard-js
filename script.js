aad74d2258f4bcc35ca83210a389211b9e8522e4
Evgeny Tunnikov <tunnikov2013@yandex.ru>
2020-07-19 23:32:25 +0300

    7 задание

--- script.js
+++ script.js
@@ -1,26 +1,15 @@
 // 1 пункт задания 
-let arr = ['23','54','46','43','13','45','27'];
-
-console.log(arr);
-
-for (let i = 0; i < arr.length; i++) {
-    let a = arr[i].indexOf('2');
-    if (arr[i].indexOf('2') === 0 || arr[i].indexOf('4') === 0) {
-        console.log(arr[i]); 
-    }
-}
-
-
-// 2 пункт задания
-
-let n = 100;
-
-nextPrime:
-for (let i = 1; i <= n; i++) {
-
-  for (let j = 2; j < i; j++) {
-    if (i % j == 0) continue nextPrime;
+let dayWeek = document.querySelectorAll('.dot'),
+  date = new Date();
+console.log(date.getDay());
+const week = ['воскресенье', 'понедельник', 'вторник','среда','четверг','пятница','суббота'];
+for (let i = 0; i < week.length; i++) {
+  dayWeek[i].textContent =  week[i];
+  if (dayWeek[i].classList.contains('dot-7') || dayWeek[i].classList.contains('dot-1')){
+    dayWeek[i].style.fontStyle = "italic";
   }
-
-  console.log( i); 
+  if ((date.getDay() )=== i) {
+    console.log(dayWeek[i]);
+    dayWeek[i].style.fontWeight = "bold"     
+  }
 }
